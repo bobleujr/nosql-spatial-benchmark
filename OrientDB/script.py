b533e5286f9369e23693ff017011d5bd82f9eabc
Paulo Tabarro <paulotabarro@geovoxel.com>
2017-02-15 13:47:30 -0500

    Orientdb loading as document

--- OrientDB/script.py
+++ OrientDB/script.py
@@ -7,25 +7,19 @@ client = pyorient.OrientDB("localhost", 2424)
 session_id = client.connect( "root", "011092" )
 
 # creating a db
-# db_name = 'GratefulDeadConcerts'
 db_name = 'gmt7004'
-print 'chegou'
 
 if not client.db_exists( db_name, pyorient.STORAGE_TYPE_MEMORY ):
 	client.db_create( db_name, pyorient.DB_TYPE_GRAPH, pyorient.STORAGE_TYPE_MEMORY )
 
 client.db_open( db_name, "root", "011092" )
 
-
-
-cluster_id = client.command( "insert into SelectedFacilities set dict = " + jsondoc )
-client.command(
-    "insert into my_class ( 'accommodation', 'work', 'holiday' ) values( 'B&B', 'garage', 'mountain' )"
-)
-
 #load file 1
 start = time.time()
 
+with open('selectedfacilities.json') as data_file:
+	for jsondoc in data_file:
+		client.command( "insert into SelectedFacilities set dict = " + jsondoc )
 
 
 end = time.time()
